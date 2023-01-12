12a87b151237dd11c0e71f617be2642e90abfeba
梁宗平 <536190198@qq.com>
2023-01-12 20:56:34 +0800

    顺序表
    错误版
    
    
    Signed-off-by: 梁宗平 <536190198@qq.com>

---
+++ seqlist1.h
@@ -0,0 +1,19 @@
+
+//ÉùÃ÷
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+
+typedef struct s {
+	int* a;
+	int size;
+	int capacity;
+}STU;
+void SeqlistInit(STU* p);
+void SeqlistPush(STU* p, int x);
+void Seqlistprint(STU* p);
+void Seqlistdestory(STU* p);
+void Seqlistcheckcapacity(STU* p);
+//void SeqlistpushFront(STU* p, int x);
